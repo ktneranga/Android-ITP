935998034310394b55579bf27adc0d22a9cf4c79
student <teran.neranga@gmail.com>
2020-02-26 16:14:16 +0530

    Commit Teran

---
+++ app/src/main/java/com/example/tutorial1/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.tutorial1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txt1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
